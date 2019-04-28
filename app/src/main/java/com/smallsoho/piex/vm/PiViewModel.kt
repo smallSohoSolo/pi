84301ee42433f4ae1a3db355c5788d73f3e58d2e
Aon <b3069741@gmail.com>
2019-04-28 21:29:27 +0800

    Feature：修改pi 的代码

--- app/src/main/java/com/smallsoho/piex/vm/PiViewModel.kt
+++ app/src/main/java/com/smallsoho/piex/vm/PiViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.jetbrains.annotations.TestOnly
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.util.concurrent.atomic.AtomicBoolean
@@ -16,10 +17,10 @@ class PiViewModel : ViewModel() {
     private var _calculateTimes = MutableLiveData<BigDecimal>()
     private var isRunning = AtomicBoolean(false)
 
-    private var _piNumber = MutableLiveData<BigDecimal>()
+    private var _piNumber = MutableLiveData<Array<BigDecimal>>()
 
     var piNumber: LiveData<String> = Transformations.map(_piNumber) {
-        it.toPlainString()
+        it[0].divide(it[1], _calculateTimes.value!!.toInt(), RoundingMode.HALF_EVEN).toPlainString()
     }
 
     var calculateTimes: LiveData<String> = Transformations.map(_calculateTimes) {
@@ -27,7 +28,7 @@ class PiViewModel : ViewModel() {
     }
 
     init {
-        _piNumber.value = BigDecimal.ZERO
+        _piNumber.value = arrayOf(BigDecimal.ZERO, BigDecimal.ONE)
         _calculateTimes.value = BigDecimal.ZERO
     }
 
@@ -45,7 +46,7 @@ class PiViewModel : ViewModel() {
 
     fun stopCalculate() {
         isRunning.set(false)
-        _piNumber.value = BigDecimal.ZERO
+        _piNumber.value = arrayOf(BigDecimal.ZERO, BigDecimal.ONE)
         _calculateTimes.value = BigDecimal.ZERO
     }
 
@@ -61,28 +62,39 @@ class PiViewModel : ViewModel() {
     private fun getNextPi() {
         if (_calculateTimes.value == BigDecimal.ZERO) {
             _calculateTimes.postValue(_calculateTimes.value?.add(BigDecimal.ONE))
-            _piNumber.postValue(BigDecimal(3))
+            _piNumber.postValue(arrayOf(BigDecimal(3), BigDecimal.ONE))
             return
         }
-        var start = BigDecimal.ONE
-        var number = BigDecimal(3)
-        while (start <= _calculateTimes.value) {
-            val flag = if (start % BigDecimal(2) == BigDecimal.ONE) {
-                BigDecimal(1)
-            } else {
-                BigDecimal(-1)
-            }
-            number += flag * BigDecimal(4).divide(
-                (start * BigDecimal(2) * (start * BigDecimal(2) + BigDecimal(
-                    1
-                )) * (start * BigDecimal(2) + BigDecimal(
-                    2
-                ))), _calculateTimes.value!!.toInt(), RoundingMode.HALF_UP
-            )
-            start++
+        val flag = if (_calculateTimes.value!! % BigDecimal(2) == BigDecimal.ONE) {
+            BigDecimal(1)
+        } else {
+            BigDecimal(-1)
         }
-        _piNumber.postValue(number)
+
+        val temp = _calculateTimes.value!! * BigDecimal(2)
+
+        _piNumber.postValue(
+            fractionAdd(
+                _piNumber.value!![0], _piNumber.value!![1],
+                BigDecimal(4) * flag , temp * (temp + BigDecimal.ONE) * (temp + BigDecimal(2))
+            )
+        )
         _calculateTimes.postValue(_calculateTimes.value?.add(BigDecimal.ONE))
+    }
+
+    private fun fractionAdd(
+        oneMolecule: BigDecimal, oneDenominator: BigDecimal,
+        twoMolecule: BigDecimal, twoDenominator: BigDecimal
+    ): Array<BigDecimal> {
+        return arrayOf(
+            oneMolecule * twoDenominator + twoMolecule * oneDenominator,
+            oneDenominator * twoDenominator
+        )
+    }
+
+    @TestOnly
+    fun testPi() {
+        getNextPi()
     }
 
 }
